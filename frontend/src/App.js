226b13ac8a1979a1fd256dc5a69be689b3157239
GP <gregpetropoulos@yahoo.com>
2022-12-12 09:26:28 -0500

    Added react-bootstrap and created a simple navbar component in the header

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,19 @@
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { Container } from 'react-bootstrap';
 
-
-const App=()=>{
+const App = () => {
   return (
-    <div className="App">
-      <h1>Welcome to Candle Shop</h1>
+    <div className='App'>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome to Candle Shop</h1>
+        </Container>
+      </main>
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
